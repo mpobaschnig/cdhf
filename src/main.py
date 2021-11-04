c9b84bab8812ef801f3a7ec3ff48cd3fd1e52701
Martin Pobaschnig <martin.pobaschnig@protonmail.com>
2021-11-04 16:01:03 +0100

    project: Initial commit

---
+++ src/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+
+
+def main() -> int:
+    """
+    Main entry point of the program.
+    """
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
