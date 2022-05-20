83712c11d35b707077699af1dd64dc0b701d026b
Igor Jakovljevic <igor.james.jakovljevic@gmail.com>
2022-05-20 16:06:47 +0200

    Improved curl for init.sh

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 mkdir -p input
-curl "https://zenodo.org/record/6319684/files/mattermost.json" -o input/mmdata.json
+curl  -o input/mmdata.json --proto '=https' --tlsv1.2 -sSf "https://zenodo.org/record/6319684/files/mattermost.json"
